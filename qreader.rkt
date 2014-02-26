ea5543d2b4b992cdcf8452f297c7d7d01b06a10e
Aaron Cowdrey <cowdrey@ku.edu>
2014-02-25 23:31:20 -0600

    Update qreader.rkt

--- qreader.rkt
+++ qreader.rkt
@@ -1,8 +1,9 @@
 ;module for storing the parser and reader functions
+#lang racket
 
 ;----------------------------------------------------------
-(define (GoodExpression? s) (not (eq? #f(andmap myRegexMatch (myRegExpSplit s)))))
-   (define (myRegExpSplit s) (regexp-split #rx"[-+]" s))
-   (define (myRegexMatch s) (regexp-match termRegex s))
-   (define termRegex #px"^(([0-9]*[i|j|k])|([0-9]))$")
+(define (GoodExpression? s) (not (eq? #f(andmap myRegexMatch (myRegExpSplit s)))))  ;Is the expression acceptable?
+   (define (myRegExpSplit s) (regexp-split #rx"[-+]" s))              ;Splits a string by "+" and "-" operaters
+   (define (myRegexMatch s) (regexp-match termRegex s))               ;Does s match termRegex
+   (define termRegex #px"^(([0-9]*[i|j|k])|([0-9]))$")                ;regex for a single term in expression
 ;----------------------------------------------------------
