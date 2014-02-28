66c2defa6d1740edd1afc0e66f999dd1e6c10124
Aaron Cowdrey <cowdrey@ku.edu>
2014-02-27 19:22:59 -0600

    Moved OldOps.rkt inside of Quaternion.rkt HA

--- Quaternion.rkt
+++ Quaternion.rkt
@@ -1,5 +1,20 @@
 #lang racket
-(require "OldOps.rkt")
+
+(module OldOps racket
+  (provide 
+   old+ old- old* old/ oldsin oldcos oldexpt oldexp oldeq?)
+  (define old+ +)
+  (define old- -)
+  (define old* *)
+  (define old/ /)
+  (define oldsin sin)
+  (define oldcos cos)
+  (define oldexp exp)
+  (define oldexpt expt)
+  (define oldeq? eq?))
+  
+(require 'OldOps.rkt)
+
 (define (+ . x)
   (if (ormap quaternion? x) (apply quaternion-add x) (apply old+ x)))
 (define (- . x)
