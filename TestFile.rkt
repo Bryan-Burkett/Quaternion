0654b4989fb3b9625c9e99be11cce9438bde0a68
JustinArnspiger <justin.arnspiger@gmail.com>
2014-02-28 18:12:50 -0600

    Update TestFile.rkt

--- TestFile.rkt
+++ TestFile.rkt
@@ -1,2 +1,11 @@
 #lang racket
 (require "Quaternion.rkt")
+
+;It was found that the reader does not function correctly when given just -i,-j, or -k:
+;>1-i-k
+;(quaternion 1 1 0 1)
+;I think that this can be fixed just by replacing a few lines of code with something like this:
+;(if (equal? AllButlastCharOfNext "") ;This is the instance that the term is a standalone i j or k
+ ;                     (if(equal? sign -1) -1           ;This assigns the minus sign when it is just -i -j or -k
+  ;                    1) 
+   ;                   (* sign (string->number AllButlastCharOfNext))))
